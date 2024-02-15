d8a57aa924cd74346c9361aeacbb57e812015846
Álvaro Morató <alvaromg_97@hotmail.com>
2024-02-15 22:27:35 +0100

    Reorganize tests

--- tests/integration/common/mod.rs
+++ tests/integration/common/mod.rs
@@ -1,57 +1,9 @@
-use std::thread;
-
 use rstest::*;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use zero2prod::{configuration, startup};
 
-#[rstest]
-#[awt]
-#[tokio::test]
-async fn health_check_returns_200(#[future] client: TestClient) {
-    let response = client.get("/health").await;
-
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
-}
-
-#[rstest]
-#[awt]
-#[tokio::test]
-async fn subscribe_returns_200(#[future] client: TestClient) {
-    let response = client
-        .post(
-            "/subscriptions",
-            "name=le%20guin&email=ursula_le_guin%40gmail.com".into(),
-        )
-        .await;
-
-    assert_status_code(200, response);
-
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
-        .fetch_one(&client.db)
-        .await
-        .expect("Failed to fetch saved subscription");
-    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
-    assert_eq!(saved.name, "le guin");
-}
-
-#[rstest]
-#[case::missing_email("name=le%20gu")]
-#[case::missing_name("email=ursula_le_guin%40gmail.com")]
-#[case::missing_all("")]
-#[awt]
-#[tokio::test]
-async fn subscribe_returns_400_when_missing_data(
-    #[future] client: TestClient,
-    #[case] body: String,
-) {
-    let response = client.post("/subscriptions", body).await;
-
-    assert_status_code(422, response);
-}
-
-async fn start_server(db: PgPool) -> String {
+pub async fn start_server(db: PgPool) -> String {
     let app = startup::app(db).await;
     let listener = startup::listener(0).await; // Random available port
     let port = listener.local_addr().unwrap().port();
@@ -60,14 +12,14 @@ async fn start_server(db: PgPool) -> String {
     format!("http://0.0.0.0:{}", port)
 }
 
-fn assert_status_code(expected: u16, response: reqwest::Response) {
+pub fn assert_status_code(expected: u16, response: reqwest::Response) {
     assert_eq!(expected, response.status().as_u16());
 }
 
-struct TestClient {
+pub struct TestClient {
     address: String,
     client: reqwest::Client,
-    db: PgPool,
+    pub db: PgPool,
 }
 
 impl TestClient {
@@ -108,7 +60,7 @@ impl TestClient {
 }
 
 #[fixture]
-async fn db() -> PgPool {
+pub async fn db() -> PgPool {
     let mut configuration =
         configuration::get_configuration().expect("Failed to read configuration");
     configuration.database.database = Uuid::new_v4().to_string();
@@ -132,7 +84,7 @@ async fn db() -> PgPool {
 
 #[fixture]
 #[awt]
-async fn client(#[future] db: PgPool) -> TestClient {
+pub async fn client(#[future] db: PgPool) -> TestClient {
     let address = start_server(db.clone()).await;
     TestClient::new(address, db)
 }
