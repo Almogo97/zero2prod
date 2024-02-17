d076899ead3fb88908214f7b09b6d4c48e29ad05
Álvaro Morató <alvaromg_97@hotmail.com>
2024-02-18 00:06:04 +0100

    Add tracing logs

--- src/startup.rs
+++ src/startup.rs
@@ -41,7 +41,7 @@ pub fn initialize_logger() {
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
                 // axum logs rejections from built-in extractors with the `axum::rejection`
                 // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
-                "example_tracing_aka_logging=debug,tower_http=debug,axum::rejection=trace".into()
+                "zero2prod=debug,tower_http=debug,axum::rejection=trace".into()
             }),
         )
         .with(tracing_subscriber::fmt::layer())
